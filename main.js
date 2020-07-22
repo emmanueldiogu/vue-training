7db3ae94a3b91f426ff90a1ba1f440fc5fa98287
emmanueldiogu <emmanueldiogu@gmail.com>
2020-07-22 23:57:46 +0100

    core vue computed properties

--- main.js
+++ main.js
@@ -1,34 +1,23 @@
 var app = new Vue({
   el: '#app',
   data: {
-    product: 'Cubic Zaconia Jewelry',
-    description: "Nice fashionable jewelry weddings and party",
-    deliveryMsg: "Your items will arrive in 1 to 3 days",
-    hideMsg: false,
-    image: {
-      url: './one.jpeg',
-      alts: 'product image'
-    },
-    links: {
-      url: "./about.html",
-      text: "About page"
-    },
-    inventory: 20,
-    inStock: true,
-    onSale: false,
+    brand: 'Cubic Zarconia',
+    product: '4 Piece Jewelry',
+    selectedVariantId: 2234,
+    onSale:true,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
     variants: [
       {
-        variantId: 2234,
-        variantColor: "green",
-        variantImage: './one.jpeg',
-        altText: "green product"
+        id: 2234,
+        color: "green",
+        image: './one.jpeg',
+        quantity: 20
       },
       {
-        varientId: 2235,
-        variantColor: "blue",
-        variantImage: './two.jpeg',
-        altText: "blue product"
+        id: 2235,
+        color: "blue",
+        image: './two.jpeg',
+        quantity: 0
       }
     ],
     cart: 0
@@ -42,9 +31,24 @@ var app = new Vue({
         this.cart -= 1
       }
     },
-    updateProduct: function (variantImage, altText, colorBox) {
-      this.image.url = variantImage
-      this.image.alts = altText
+    updateProduct: function (id) {
+      this.selectedVariantId = id
+    }
+  },
+  computed: {
+    title: function () {
+      if (this.onSale) {
+        return this.brand + ' ' + this.product
+      }
+    },
+    selectedVariant() {
+      return this.variants.find(v=> v.id == this.selectedVariantId)
+    },
+    image() {
+      return this.selectedVariant.image
+    },
+    inStock() {
+      return this.selectedVariant.quantity
     }
   }
 })
